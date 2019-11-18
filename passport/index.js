8e00274ad015282f588ef3fcc4f91f735253fe65
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-11-18 15:47:19 +0100

    routes - index, profile, kitaDetail

--- passport/index.js
+++ passport/index.js
@@ -12,3 +12,74 @@ module.exports = (app) => {
   app.use(passport.initialize());
   app.use(passport.session());
 }
+
+/* GET home page */
+router.get("/", (req, res) => {
+  res.render("index", {
+    loggedIn: req.user
+  });
+});
+
+const loginCheck = () => {
+  return (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  };
+};
+
+router.get("/profile", loginCheck(), (req, res, next) => {
+  Room.find({
+      owner: req.user._id
+    })
+    .then(rooms => {
+      res.render("profile.hbs", {
+        user: req.user,
+        rooms: rooms
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+router.post("/kitaDetail/:kitaId/comment", loginCheck(), (req, res, next) => {
+  const content = req.body.comment;
+  const author = req.user._id;
+
+  Comment.create({
+      content: content,
+      author: author
+    })
+    .then(comment => {
+      return Kita.findOneAndUpdate({
+          _id: req.params.kitaId
+        }, {
+          $push: {
+            comments: comment._id
+          }
+        }, {
+          new: true
+        })
+        .populate({
+          path: "comments", // populates the `comments` field in the Room
+          populate: {
+            path: "author" // populates the `author` field in the Comment
+          }
+        })
+        .then(kita => {
+          res.json(kita.comments); // updated comments array
+
+          // send the room's document
+          // res.redirect(`/rooms/${req.params.roomId}`);
+        });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
