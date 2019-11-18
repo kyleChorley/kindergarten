5934abb86671d396abe304dfc2760fd1d12993bc
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-11-18 18:44:33 +0100

    stays logged in

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
-  console.log();
+  res.render('index', {
+    loggedIn: req.user
+  });
 });
 
 module.exports = router;
