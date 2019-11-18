a24088d7687e2821b96822286101bd4dbc5fa65f
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-11-18 20:46:34 +0100

    rough kita checklist

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,30 @@ router.get('/', (req, res, next) => {
   });
 });
 
+
+const loginCheck = () => {
+  return (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  };
+};
+
+router.get("/profile", loginCheck(), (req, res, next) => {
+  Room.find({
+      owner: req.user._id
+    })
+    .then(rooms => {
+      res.render("profile.hbs", {
+        user: req.user,
+        rooms: rooms
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
