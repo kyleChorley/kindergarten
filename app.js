4d19e756288e701d16dc234ca95f52ce694bfb6f
toskaMaliske <toska.maliske@icloud.com>
2019-11-18 11:48:16 +0100

    initial commit map

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/week6', {useNewUrlParser: true})
+  .connect('mongodb://localhost/kindergarten', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
