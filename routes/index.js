c5a4dbd738f427782aeb011c64b861b2dd348161
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-11-19 16:44:15 +0100

    comments enabled: kita and profile

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const Kita = require("../models/Kita");
+const User = require("../models/User");
+const Comment = require("../models/Comment");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -20,18 +23,81 @@ const loginCheck = () => {
 };
 
 router.get("/profile", loginCheck(), (req, res, next) => {
-  Room.find({
-      owner: req.user._id
-    })
-    .then(rooms => {
+  console.log(req.user)
+  Comment.find({
+      author: req.user._id
+    }).populate('kita')
+    .then(response => {
       res.render("profile.hbs", {
+        comments: response,
+        loggedIn: req.user,
         user: req.user,
-        rooms: rooms
+        kitas: req.user.kitas
       });
     })
     .catch(err => {
       next(err);
     });
 });
+
+
+router.get("/:kitaId", loginCheck(), (req, res, next) => {
+  Kita.findById(req.params.kitaId)
+    .populate({
+      path: "comments", // populates the `comments` field in the Kita
+      populate: {
+        path: "author" // populates the `author` field in the Comment
+      }
+    })
+    .then(kita => {
+      console.log(kita)
+      res.render("kitaDetail.hbs", {
+        kita: kita,
+        loggedIn: req.user
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+router.post("/kitaDetail/:kitaId/comment", loginCheck(), (req, res, next) => {
+  const content = req.body.comment;
+  const author = req.user._id;
+
+  Comment.create({
+      content: content,
+      author: author,
+      kita: req.params.kitaId
+    })
+    .then(comment => {
+      return Kita.findOneAndUpdate({
+          _id: req.params.kitaId
+        }, {
+          $push: {
+            comments: comment._id
+          }
+        }, {
+          new: true
+        })
+        .populate({
+          path: "comments", // populates the `comments` field in the Kita
+          populate: {
+            path: "author" // populates the `author` field in the Comment
+          }
+        })
+        .then(something => {
+          console.log(something)
+          res.json(something.comments); // updated comments array
+
+        });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
 
 module.exports = router;
