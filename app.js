7937a828ce8e1cd3cffe0fc1d6b2159f2a4e8943
toskaMaliske <toska.maliske@icloud.com>
2019-11-20 22:02:21 +0100

    First bootstrap styling

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 const session = require("express-session");
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
-
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/kindergarten', {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/kindergarten", {
     useNewUrlParser: true
   })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-hbs.registerHelper('ifUndefined', (value, options) => {
+hbs.registerHelper("ifUndefined", (value, options) => {
   if (arguments.length < 2)
     throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
   if (typeof value !== undefined) {
@@ -63,26 +68,26 @@ hbs.registerHelper('ifUndefined', (value, options) => {
   }
 });
 
-hbs.registerHelper("json", (obj) => {
-  return new hbs.SafeString(JSON.stringify(obj))
+hbs.registerHelper("json", obj => {
+  return new hbs.SafeString(JSON.stringify(obj));
 });
 
 // default value for title local
-app.locals.title = 'Kindergarten';
-
+app.locals.title = "Kindergarten";
 
 // Enable authentication using session + passport
-app.use(session({
-  secret: 'irongenerator',
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection
+app.use(
+  session({
+    secret: "irongenerator",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    })
   })
-}))
+);
 app.use(flash());
-require('./passport')(app);
-
+require("./passport")(app);
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -108,8 +113,8 @@ const bcrypt = require("bcrypt");
 passport.use(
   new LocalStrategy((username, password, done) => {
     User.findOne({
-        username: username
-      })
+      username: username
+    })
       .then(user => {
         if (!user) {
           done(null, false, {
@@ -165,7 +170,6 @@ passport.use(
 //   )
 // );
 
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -190,7 +194,7 @@ app.locals.title = "Kindergarten";
 const index = require("./routes/index");
 app.use("/", index);
 
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
 module.exports = app;
