b234ede9a7e9ead0c1b29079d24403a5557f6cff
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-11-21 21:43:05 +0100

    Change back to just heroku google callback

--- app.js
+++ app.js
@@ -140,12 +140,13 @@ passport.use(
 );
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// "http://localhost:3000/auth/google/callback"
 
 passport.use(
   new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback"
+      callbackURL: "https://kita-search.herokuapp.com/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({
