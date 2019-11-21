13df653c3cf4fc1b7561c8f02ba953e0657bcbb0
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-11-21 20:57:55 +0100

    callbackUrl fix

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ passport.use(
   new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: ("https://kita-search.herokuapp.com/" || "http://localhost:3000/auth/google/callback")
+      callbackURL: ("https://kita-search.herokuapp.com/auth/google/callback" || "http://localhost:3000/auth/google/callback")
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({
